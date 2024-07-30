bc4a76f77dfb1d95771ac1cbebbda06eca7fba49
ranujap00 <ranujap00@gmail.com>
2024-07-31 00:27:45 +0530

    increase the payload size

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,9 @@ app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Routes
 app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
