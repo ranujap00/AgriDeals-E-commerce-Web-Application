c16df743db20a501da9b560de6a3129ce42a74ee
ranujap00 <ranujap00@gmail.com>
2024-07-31 00:33:08 +0530

    update payload fix

--- Backend/app.js
+++ Backend/app.js
@@ -14,10 +14,8 @@ const app = express();
 app.use(cors());
 
 // Middleware
-app.use(bodyParser.json());
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use('/api/orders', orderRoutes);
